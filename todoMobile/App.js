47378e52070fc5af57b76a7889b430a1014e14ac
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-16 23:17:21 +0300

    mobile app | updated some files

--- todoMobile/App.js
+++ todoMobile/App.js
@@ -1,35 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList } from 'react-native'
-import axios from 'axios'
+import React from 'react'
+import Router from './src/Router'
 
 const App = () => {
-
-  const [todoList, setTodoList] = useState([])
-
-
-  useEffect(() => {
-    fetcData()
-  }, [])
-
-  const fetcData = async () => {
-    const { data } = await axios.get('http://10.0.3.2:8000/todos/')
-    // console.log(data.results)
-
-    setTodoList(data.results)
-  }
-
-  const renderTodoList = ({item}) => <Text>{item.title}</Text>
   return (
-    <View>
-
-    <Text>Merhaba</Text>
-
-    <FlatList 
-      data={todoList}
-      renderItem={renderTodoList}
-    />
-      
-    </View>
+    <Router/>
   )
 }
 
